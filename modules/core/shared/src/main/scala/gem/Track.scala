6c5a56adbef2098458192c9d3c5323c0099f0d7f
Rob Norris <rob_norris@mac.com>
2017-09-06 10:41:25 -0700

    initial simple target model

---
+++ modules/core/shared/src/main/scala/gem/Track.scala
@@ -0,0 +1,35 @@
+// Copyright (c) 2016-2017 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package gem
+
+import gem.enum.Site
+import gem.math._
+import java.time.Instant
+
+/**
+ * Time/site-parameterized coordinates over a span of time. This generalizes proper motion and
+ * ephemerides.
+ */
+sealed trait Track {
+  def at(time: Instant, site: Site): Option[Coordinates]
+}
+
+object Track {
+
+  final case class Sidereal(properMotion: ProperMotion) extends Track {
+    override def at(time: Instant, site: Site) =
+      Some(properMotion.at(time).baseCoordinates)
+  }
+
+  final case class Nonsidereal(ephemerisKey: EphemerisKey, ephemerides: Map[Site, Ephemeris]) extends Track {
+
+    override def at(time: Instant, s: Site) =
+      ephemeris(s).flatMap(_.get(time))
+
+    def ephemeris(s: Site): Option[Ephemeris] =
+      ephemerides.get(s)
+
+  }
+
+}
